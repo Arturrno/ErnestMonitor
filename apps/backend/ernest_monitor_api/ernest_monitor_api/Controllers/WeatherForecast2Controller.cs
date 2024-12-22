5b9584141f6c00b8487c892a8b7f0add156234f0
Artur <mrarturno@gmail.com>
2024-12-22 14:17:56 +0100

    backend api9

---
+++ apps/backend/ernest_monitor_api/ernest_monitor_api/Controllers/WeatherForecast2Controller.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ernest_monitor_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecast2Controller : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecast2Controller> _logger;
+
+        public WeatherForecast2Controller(ILogger<WeatherForecast2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetDupa")]
+        public string GetDupa()
+        {
+            return "dupa";
+        }
+    }
+}
