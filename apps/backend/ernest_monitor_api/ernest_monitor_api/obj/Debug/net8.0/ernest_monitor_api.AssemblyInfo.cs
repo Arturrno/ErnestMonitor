9847ccf5378ca71fdb58c20d3e6b8bb9fc467050
bartekmalinowski <bm306631@student.polsl.pl>
2025-01-10 15:39:52 +0100

    Home Page

--- apps/backend/ernest_monitor_api/ernest_monitor_api/obj/Debug/net8.0/ernest_monitor_api.AssemblyInfo.cs
+++ apps/backend/ernest_monitor_api/ernest_monitor_api/obj/Debug/net8.0/ernest_monitor_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ernest_monitor_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abde8f3f6d75dc01c09640ff4d9e6f7e7a1098e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276effca71356a233c6da70152ad48a0d94bc602")]
 [assembly: System.Reflection.AssemblyProductAttribute("ernest_monitor_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ernest_monitor_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
