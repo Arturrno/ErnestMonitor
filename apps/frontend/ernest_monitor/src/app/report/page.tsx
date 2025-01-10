7ef4e4fb81cd64a66e04547399dddca9fba9a29f
antosiowsky <115071865+antosiowsky@users.noreply.github.com>
2025-01-10 14:17:44 +0100

    threat and resource form done

---
+++ apps/frontend/ernest_monitor/src/app/report/page.tsx
@@ -0,0 +1,18 @@
+"use client"; 
+
+import React from 'react';
+import NavScroll from '../components/NavScroll';
+import ReportThreatForm from '../components/ReportThreatForm';
+
+const ReportPage = () => {
+  return (
+    <div >
+      <NavScroll />
+      <ReportThreatForm />
+    </div>
+  );
+};
+
+
+
+export default ReportPage;
---
+++ apps/frontend/ernest_monitor/src/app/resource/page.tsx
@@ -0,0 +1,18 @@
+"use client"; 
+
+import React from 'react';
+import NavScroll from '../components/NavScroll';
+import ReportResourceForm from '../components/ReportResourceForm';
+
+const ReportResourcePage = () => {
+  return (
+    <div >
+      <NavScroll />
+      <ReportResourceForm />
+    </div>
+  );
+};
+
+
+
+export default ReportResourcePage;
