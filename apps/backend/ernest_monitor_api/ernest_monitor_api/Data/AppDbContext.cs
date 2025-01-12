485e75101892220768a8e4e34fdccc03fec2dcff
Cegla <kamilceglarski2002@gmail.com>
2025-01-12 22:30:57 +0100

    initial configuration with database (not working)

---
+++ apps/backend/ernest_monitor_api/ernest_monitor_api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿// Data/AppDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using SampleAzureApi.Models;
+
+namespace SampleAzureApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Threads> Threads { get; set; }
+    }
+}
